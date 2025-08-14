ea5fd5412a1216758c83c92e51d3feef97c3c64b
charles <chandraprakashwork@outlook.com>
2025-08-14 01:29:51 +0100

    Initial commit

---
+++ src/main/java/com/SpringBootBroadcomAPI/SpringBootBroadcomApiApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBootBroadcomAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBroadcomApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBroadcomApiApplication.class, args);
+	}
+
+}
